c32fcaf75b0a4091694da744f34bcb7f745cf9d7
Dev Mukherjee <devraj@gmail.com>
2016-09-22 16:42:55 +1000

    Logos and Javascript files added

---
+++ js/locations.js
@@ -0,0 +1,210 @@
+locations = [
+    {
+        town: "Berrigan",
+        name: "Berrigan Landfill",
+        address: "Greenhills Road, Berrigan",
+        hours: "Tuesday to Thursday 1:30-4pm, Sunday 12:30-4pm",
+        contact: "03 5888 5100",
+        more_info: "http://www.berriganshire.nsw.gov.au/Residents/GarbageRecycling.aspx"
+    },
+    {
+        town: "Blighty",
+        name: "Blighty Landfill",
+        address: "Riverina Highway, Blighty",
+        hours: "Contact council for opening hours",
+        contact: "03 5880 1200",
+        more_info: "http://www.conargo.nsw.gov.au/environment/waste-and-recycling"
+    },
+    {
+        town: "Coleambally",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    },
+    {
+        town: "",
+        name: "",
+        address: "",
+        hours: "",
+        contact: "",
+        more_info: ""
+    }
+]
